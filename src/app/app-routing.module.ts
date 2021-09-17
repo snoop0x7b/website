667d51d03d0cf01c9b24ed6b0bfe63241aeedc8f
Zach Stein <zstein@holts.com>
2021-09-16 21:15:07 -0400

    Set home to not redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {ResumeComponent} from "./resume/resume.component";
 import {WoodComponent} from "./wood/wood.component";
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", redirectTo: "home" },
+  { path: "", pathMatch: "full", component: HomeComponent },
   { path: "home", component: HomeComponent},
   { path: "resume", component: ResumeComponent},
   { path: "woodwork", component: WoodComponent }
